6905a48853b057c6ee2e9157491a672878ee4de4
Dave Enrico <de269@njit.edu>
2026-04-14 15:52:36 -0400

    Modify database_init to incorporate Calculation table

--- app/database_init.py
+++ app/database_init.py
@@ -1,6 +1,9 @@
 from app.database import engine
 from app.models.user import Base
 
+# ensure the Calculation table is registered before create_all is called.
+from app.models.calculation import Calculation 
+
 def init_db():
     Base.metadata.create_all(bind=engine)
 
